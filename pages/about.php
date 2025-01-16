7df87578fe0a7a309c33a9da21ffd1028ed796c8
JOSEPH-MARIE Julien <12202288@q10409.iutv.univ-paris13.fr>
2025-01-16 11:59:32 +0100

    maj4

--- pages/about.php
+++ pages/about.php
@@ -13,13 +13,13 @@
         </h2>
         <div id="collapseFrontend" class="accordion-collapse collapse show" aria-labelledby="headingFrontend" data-bs-parent="#skillsAccordion">
             <div class="accordion-body">
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>Bootstrap</li>
-                    <li>React</li>
-                    <li>Vue.js</li>
+                <ul class="list-unstyled">
+                    <li><img src="../assets/images/html.png" alt="HTML" class="me-2" style="width: 20px;"> HTML</li>
+                    <li><img src="../assets/images/css.png" alt="CSS" class="me-2" style="width: 20px;"> CSS</li>
+                    <li><img src="../assets/images/javascript.png" alt="JavaScript" class="me-2" style="width: 20px;"> JavaScript</li>
+                    <li><img src="../assets/images/bootstrap.png" alt="Bootstrap" class="me-2" style="width: 20px;"> Bootstrap</li>
+                    <li><img src="../assets/images/react.png" alt="React" class="me-2" style="width: 20px;"> React</li>
+                    <li><img src="../assets/images/vuejs.png" alt="Vue.js" class="me-2" style="width: 20px;"> Vue.js</li>
                 </ul>
             </div>
         </div>
@@ -34,13 +34,11 @@
         </h2>
         <div id="collapseBackend" class="accordion-collapse collapse" aria-labelledby="headingBackend" data-bs-parent="#skillsAccordion">
             <div class="accordion-body">
-                <ul>
-                    <li>PHP</li>
-                    <li>Node.js</li>
-                    <li>Python (Django, Flask)</li>
-                    <li>Ruby on Rails</li>
-                    <li>Java (Spring Boot)</li>
-                    <li>C# (.NET)</li>
+                <ul class="list-unstyled">
+                    <li><img src="../assets/images/php.png" alt="PHP" class="me-2" style="width: 20px;"> PHP</li>
+                    <li><img src="../assets/images/nodejs.png" alt="Node.js" class="me-2" style="width: 20px;"> Node.js</li>
+                    <li><img src="../assets/images/python.png" alt="Python" class="me-2" style="width: 20px;"> Python</li>
+                    <li><img src="../assets/images/java.png" alt="Java" class="me-2" style="width: 20px;"> Java</li>
                 </ul>
             </div>
         </div>
@@ -55,13 +53,13 @@
         </h2>
         <div id="collapseOthers" class="accordion-collapse collapse" aria-labelledby="headingOthers" data-bs-parent="#skillsAccordion">
             <div class="accordion-body">
-                <ul>
-                    <li>SQL (MySQL, PostgreSQL)</li>
-                    <li>NoSQL (MongoDB, Firebase)</li>
-                    <li>Git & GitHub</li>
-                    <li>Docker</li>
-                    <li>CI/CD (Jenkins, GitLab CI)</li>
-                    <li>Linux</li>
+                <ul class="list-unstyled">
+                    <li><img src="../assets/images/sql.png" alt="SQL" class="me-2" style="width: 20px;"> SQL (MySQL, PostgreSQL)</li>
+                    <li><img src="../assets/images/nosql1.png" alt="NoSQL" class="me-2" style="width: 20px;"> NoSQL (MongoDB, Redis)</li>
+                    <li><img src="../assets/images/git.png" alt="Git" class="me-2" style="width: 20px;"> Git & GitHub</li>
+                    <li><img src="../assets/images/docker.png" alt="Docker" class="me-2" style="width: 20px;"> Docker</li>
+                    <li><img src="../assets/images/jenkins.png" alt="CI/CD" class="me-2" style="width: 20px;"> CI/CD (Jenkins)</li>
+                    <li><img src="../assets/images/linux.png" alt="Linux" class="me-2" style="width: 20px;"> Linux</li>
                 </ul>
             </div>
         </div>

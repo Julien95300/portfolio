f0014c617d9e96623bce78439e12d2f084cfa990
Julien <julienjm07@gmail.com>
2025-02-25 14:43:02 +0100

    maj

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,7 @@
 <?php include('../includes/header.php'); ?>
-<h1>Bienvenue sur mon portfolio</h1>
+<h1>Bienvenue sur le portfolio de JOSEPH MARIE Julien </h1>
+<p> Vous voulez en savoir plus sur moi ? </p>
+<a href="about.php" class="btn btn-primary">A propos de moi</a>
 <p>Découvrez mes projets et compétences.</p>
 <a href="portfolio.php" class="btn btn-primary">Voir mes projets</a>
 <?php include('../includes/footer.php'); ?>
--- pages/portfolio.php
+++ pages/portfolio.php
@@ -1,16 +1,70 @@
 <?php include('../includes/header.php'); ?>
+
 <h1>Mes Projets</h1>
 <div class="row">
+    <!-- Première carte -->
     <div class="col-md-4">
         <div class="card">
             <img src="../assets/images/projet1.jpg" class="card-img-top" alt="Projet 1">
             <div class="card-body">
-                <h5 class="card-title">Projet 1</h5>
-                <p class="card-text">Description du projet 1.</p>
-                <a href="" class="btn btn-primary">Voir plus</a>
+                <h5 class="card-title">Site de formations - Perform_Vision</h5>
+                <p class="card-text">Perform_Vision est une plateforme web développée pour l'entreprise SAS Perform Vision, spécialisée dans les services de formation. Le site permet aux formateurs de s'inscrire, déclarer leurs compétences, et répondre aux questions des clients, tout en facilitant la mise en relation entre eux. Les utilisateurs peuvent rechercher des formateurs selon leurs compétences et trouver des formations adaptées à leurs besoins.</p>
+                <p><strong>Technologies utilisées :</strong></p>
+                <p>
+                    <img src="../assets/images/php.png" alt="PHP" style="width: 30px; height: 30px; margin-right: 10px;">
+                    <img src="../assets/images/sql.png" alt="MySQL" style="width: 30px; height: 30px; margin-right: 10px;">
+                    <img src="../assets/images/css.png" alt="css" style="width: 30px; height: 30px; margin-right: 10px;">
+                    <img src="../assets/images/html.png" alt="html" style="width: 30px; height: 30px; margin-right: 10px;">
+
+                </p>
+                <a href="https://github.com/Julien95300/PerformVision" class="btn btn-primary">
+                    <img src="../assets/images/github.png" alt="GitHub Logo" style="width: 50px; height: 30px;">
+                </a>
             </div>
         </div>
     </div>
-    <!-- Ajouter d'autres projets -->
+
+    <!-- Deuxième carte -->
+    <div class="col-md-4">
+        <div class="card">
+            <img src="../assets/images/flixtaneuse.png" class="card-img-top" alt="Projet 2">
+            <div class="card-body">
+                <h5 class="card-title">Flixtaneuse</h5>
+                <p class="card-text">Flixtaneuse est une plateforme moderne pour les passionnés de cinéma. Le site permet d'explorer, rechercher et organiser des films avec des fonctionnalités telles que la recherche avancée et les classements personnalisés. Un sélecteur multimodal interactif permet à l'utilisateur de rechercher des films de manière fluide en combinant différents critères comme le titre, le genre, et plus encore.</p>
+                <p><strong>Technologies utilisées :</strong></p>
+                <p>
+                    <strong>Frontend :</strong> <img src="../assets/images/vuejs.png" alt="Vue.js" style="width: 50px; height: 30px; margin-right: 10px;">
+                    <strong>Backend :</strong> <img src="../assets/images/nodejs.png" alt="Node.js" style="width: 50px; height: 30px; margin-right: 10px;">
+                    <img src="../assets/images/expressjs.png" alt="Express.js" style="width: 60px; height: 30px; margin-right: 10px;"><br>
+                    <strong>Base de données :</strong> <img src="../assets/images/nosql2.png" alt="MongoDB" style="width: 60px; height: 30px; margin-right: 10px;">
+                </p>
+                <a href="https://github.com/romainwgr/Flixtaneuse" class="btn btn-primary">
+                    <img src="../assets/images/github.png" alt="GitHub Logo" style="width: 50px; height: 30px;">
+                </a>
+                <a href="https://flixtaneuse.vercel.app/" class="btn btn-primary">
+                    <img src="../assets/images/web.png" alt="Site" style="width: 30px; height: 30px;">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Troisième carte -->
+    <div class="col-md-4">
+        <div class="card">
+            <img src="../assets/images/projet1.jpg" class="card-img-top" alt="Projet 3">
+            <div class="card-body">
+                <h5 class="card-title">Site de formations</h5>
+                <p class="card-text">Un site web permettant de mettre en relation des formateurs avec des utilisateurs pour des sessions de formation en ligne.</p>
+                <p><strong>Technologies utilisées :</strong></p>
+                <p>
+                    <img src="../assets/images/php-logo.png" alt="PHP" style="width: 30px; height: 30px; margin-right: 10px;"> PHP
+                </p>
+                <a href="https://github.com/Julien95300/PerformVision" class="btn btn-primary">
+                    <img src="../assets/images/github.png" alt="GitHub Logo" style="width: 50px; height: 30px;">
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
+
 <?php include('../includes/footer.php'); ?>
